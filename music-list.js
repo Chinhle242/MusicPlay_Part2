fcb89d07e03b8083d4099ad02add99b46cb41e43
Chinhle242 <96275096+Chinhle242@users.noreply.github.com>
2025-11-17 01:34:24 +0700

    Update music-list.js

--- music-list.js
+++ music-list.js
@@ -9,12 +9,42 @@
 
 let allMusic = [
   {
-    name: "Đám Cưới Nha",
-    artist: "Hồng Thanh,Mie",
-    img: "Dam-Cuoi-Nha",
-    src: "Dam-cuoi-nha"
+    name: "Gặp Lại Năm Ta 60",
+    artist: "ORAGRE",
+    img: "Gặp Lại Năm Ta 60",
+    src: "Gặp Lại Năm Ta 60"
+  },{
+    name: "Ngày Tháng Sau Này",
+    artist: "Lillie",
+    img: "Ngày Tháng Sau Này",
+    src: "Ngày Tháng Sau Này"
+  },{
+    name: "Vùng An Toàn",
+    artist: "bray",
+    img: "Vùng An Toàn",
+    src: "Vùng An Toàn"
+  }, {
+    name: "Nếu Những Tiếc Nuối",
+    artist: "Vũ",
+    img: "Nếu Những Tiếc Nuối",
+    src: "Nếu Những Tiếc Nuối"
   },
- 
+ {
+    name: "Phép Màu",
+    artist: "mounter",
+    img: "Phép Màu",
+    src: "Phép Màu"
+  },{
+    name: "WRONG TIMES",
+    artist: "PUPPY VS DANGRANGTO",
+    img: "WRONG TIMES",
+    src: "WRONG TIMES"
+  }, {
+    name: "Lựa Chọn Của em",
+    artist: "TRUONGLINH PHAMTIEN",
+    img: "Lựa Chọn Của em",
+    src: "Lựa Chọn Của em"
+  },
   {
     name: "See Tình",
     artist: "Hoàng Thùy Linh",
@@ -26,18 +56,39 @@ let allMusic = [
     artist: "Huy Vạc",
     img: "ChiMuonBenEmLucNay",
     src: "ChiMuonBenEmLucNay"
-  },
-  {
+  },{
     name: "Hồng Nhan",
     artist: "Jack",
     img: "Hong-Nhan-Jack",
     src: "Hong-Nhan-Jack"
-  },
-  {
+  }, {
     name: "Chỉ Muốn Bên Em Thật Gần",
     artist: "YLinh,Xám",
     img: "Chi-Muon-Ben-Em-That-Gan",
     src: "Chi-Muon-Ben-Em-That-Gan"
+  },{
+    name: "{
+    name: "Em - Binz",
+    artist: "BINZ",
+    img: "Em - Binz",
+    src: "Em - Binz"
+  },
+  {
+    name: "In Love",
+    artist: "LOW G",
+    img: "In Love",
+    src: "In Love"
+  },
+  {
+    name: "Giờ Thì",
+    artist: "TRUONGLINH",
+    img: "Giờ Thì",
+    src: "Giờ Thì"
+  }, {
+    name: "Từng Ngày Yêu Em",
+    artist: "TRUONGLINH",
+    img: "Từng Ngày Yêu Em",
+    src: "Từng Ngày Yêu Em"
   },
   {
     name: "Em Là Nhất",
@@ -46,9 +97,22 @@ let allMusic = [
     src: "Em-La-Nhat"
   },
   {
-    name: "Vì anh là gió",
-    artist: "Lilk",
-    img: "ViAnhLaGio-LilK-4766315",
-    src: "ViAnhLaGio-LilK-4766315"
+    name: "Già cùng nhau là được",
+    artist: "TEA",
+    img: "Già cùng nhau là được",
+    src: "Già cùng nhau là được"
   },
+  {
+    name: "GHÉ QUA",
+    artist: "DICK",
+    img: "GHÉ QUA",
+    src: "GHÉ QUA"
+  },
+  {
+    name: "Nụ hôn Bisou",
+    artist: "MIKELOGIC",
+    img: "Nụ hôn Bisou",
+    src: "Nụ hôn Bisou"
+  },
+
 ];
