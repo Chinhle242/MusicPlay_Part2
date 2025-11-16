abc478d20319a5ace31c1e400286c9d7cceab2c3
Chinhle242 <96275096+Chinhle242@users.noreply.github.com>
2025-11-17 02:14:18 +0700

    Update music-list.js

--- music-list.js
+++ music-list.js
@@ -1,49 +1,45 @@
-// Để thêm bài nhạc mình sẽ them vào list sau
-
-//   {
-//     name: "Tên bài nhạc",
-//     artist: "Nghệ sĩ",
-//     img: "Tên ảnh định dạng .jpg trong project",
-//     src: "Link đường dẫn file nhạc đinh dạng .mp3"
-//   }
-
 let allMusic = [
   {
     name: "Gặp Lại Năm Ta 60",
     artist: "ORAGRE",
-    img: "Gặp Lại Năm Ta 60",
-    src: "Gặp Lại Năm Ta 60"
-  },{
+    img: "Gặp-Lại-Năm-Ta-60",
+    src: "Gặp-Lại-Năm-Ta-60"
+  },
+  {
     name: "Ngày Tháng Sau Này",
     artist: "Lillie",
-    img: "Ngày Tháng Sau Này",
-    src: "Ngày Tháng Sau Này"
-  },{
+    img: "Ngày-Tháng-Sau-Này",
+    src: "Ngày-Tháng-Sau-Này"
+  },
+  {
     name: "Vùng An Toàn",
     artist: "bray",
-    img: "Vùng An Toàn",
-    src: "Vùng An Toàn"
-  }, {
+    img: "Vùng-An-Toàn",
+    src: "Vùng-An-Toàn"
+  },
+  {
     name: "Nếu Những Tiếc Nuối",
     artist: "Vũ",
-    img: "Nếu Những Tiếc Nuối",
-    src: "Nếu Những Tiếc Nuối"
+    img: "Nếu-Những-Tiếc-Nuối",
+    src: "Nếu-Những-Tiếc-Nuối"
   },
- {
+  {
     name: "Phép Màu",
     artist: "mounter",
-    img: "Phép Màu",
-    src: "Phép Màu"
-  },{
+    img: "Phép-Màu",
+    src: "Phép-Màu"
+  },
+  {
     name: "WRONG TIMES",
     artist: "PUPPY VS DANGRANGTO",
-    img: "WRONG TIMES",
-    src: "WRONG TIMES"
-  }, {
-    name: "Lựa Chọn Của em",
+    img: "WRONG-TIMES",
+    src: "WRONG-TIMES"
+  },
+  {
+    name: "Lựa Chọn Của Em",
     artist: "TRUONGLINH PHAMTIEN",
-    img: "Lựa Chọn Của em",
-    src: "Lựa Chọn Của em"
+    img: "Lựa-Chọn-Của-Em",
+    src: "Lựa-Chọn-Của-Em"
   },
   {
     name: "See Tình",
@@ -54,41 +50,44 @@ let allMusic = [
   {
     name: "Chỉ Muốn Bên Em Lúc Này",
     artist: "Huy Vạc",
-    img: "ChiMuonBenEmLucNay",
-    src: "ChiMuonBenEmLucNay"
-  },{
+    img: "Chi-Muon-Ben-Em-Luc-Nay",
+    src: "Chi-Muon-Ben-Em-Luc-Nay"
+  },
+  {
     name: "Hồng Nhan",
     artist: "Jack",
     img: "Hong-Nhan-Jack",
     src: "Hong-Nhan-Jack"
-  }, {
+  },
+  {
     name: "Chỉ Muốn Bên Em Thật Gần",
     artist: "YLinh,Xám",
     img: "Chi-Muon-Ben-Em-That-Gan",
     src: "Chi-Muon-Ben-Em-That-Gan"
-  },{
-   
+  },
+  {
     name: "Em - Binz",
     artist: "BINZ",
-    img: "Em - Binz",
-    src: "Em - Binz"
+    img: "Em-Binz",
+    src: "Em-Binz"
   },
   {
     name: "In Love",
     artist: "LOW G",
-    img: "In Love",
-    src: "In Love"
+    img: "In-Love",
+    src: "In-Love"
   },
   {
     name: "Giờ Thì",
     artist: "TRUONGLINH",
-    img: "Giờ Thì",
-    src: "Giờ Thì"
-  }, {
+    img: "Gio-Thi",
+    src: "Gio-Thi"
+  },
+  {
     name: "Từng Ngày Yêu Em",
     artist: "TRUONGLINH",
-    img: "Từng Ngày Yêu Em",
-    src: "Từng Ngày Yêu Em"
+    img: "Tung-Ngay-Yeu-Em",
+    src: "Tung-Ngay-Yeu-Em"
   },
   {
     name: "Em Là Nhất",
@@ -97,23 +96,21 @@ let allMusic = [
     src: "Em-La-Nhat"
   },
   {
-    name: "Già cùng nhau là được",
+    name: "Già Cùng Nhau Là Được",
     artist: "TEA",
-    img: "Già cùng nhau là được",
-    src: "Già cùng nhau là được"
+    img: "Gia-Cung-Nhau-La-Duoc",
+    src: "Gia-Cung-Nhau-La-Duoc"
   },
   {
     name: "GHÉ QUA",
     artist: "DICK",
-    img: "GHÉ QUA",
-    src: "GHÉ QUA"
+    img: "GHE-QUA",
+    src: "GHE-QUA"
   },
   {
-    name: "Nụ hôn Bisou",
+    name: "Nụ Hôn Bisou",
     artist: "MIKELOGIC",
-    img: "Nụ hôn Bisou",
-    src: "Nụ hôn Bisou"
-  },
-
+    img: "Nu-Hon-Bisou",
+    src: "Nu-Hon-Bisou"
+  }
 ];
-
